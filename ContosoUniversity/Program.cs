6426f4188f06bcbe4276621d2aeca5b1a073e9bf
Souhaila-nouinou <souhaila.nouinou22@gmail.com>
2022-06-13 18:18:58 +0100

    Create an initial migration

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -35,7 +35,7 @@ using (var scope = app.Services.CreateScope())
     var services = scope.ServiceProvider;
 
     var context = services.GetRequiredService<SchoolContext>();
-    context.Database.EnsureCreated();
+    // context.Database.EnsureCreated();
     DbInitializer.Initialize(context);
 }
 #endregion
